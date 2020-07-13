7fde3a39dfbc9bfd4f5a197ce37f19b1b54615a9
Rishit Gupta <31159803+rishitgupta@users.noreply.github.com>
2020-07-13 18:50:08 +0530

    Added basic commands

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+# Excellent/Ah, Noice/Nice, Cool, Toit, Woooooo/Woohoo (Ted), nikal, knockknockknock
+
 import discord
 from discord.ext import commands
+
+import random
 
 client = commands.Bot(command_prefix = "")
 
 @client.event
 async def on_ready():
     print("Bot is ready.")
+
+@client.command(aliases=["whatistheping", "whatstheping", "wotisleping"])
+async def ping(ctx):
+    await ctx.send(f"Pong! The ping is {round(client.latency * 1000)}ms.")
+
+@client.command(aliases=["8ball"])
+async def _8ball(ctx, *, question):
+    responses = ["_Sure, honey, sure._", "You really need to ask?", "Of course, bruv.", "Bhai mai bhagwaan nahi hoon", ""]
+    await ctx.send(random.choice(responses))
+
 @client.command()
 async def edith(ctx):
-    await ctx.send("teri maa ki- chup")
-client.run("")
+    await ctx.send("chup ho jaa")
